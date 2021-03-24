80b5d6928221108027d09baa4adf87b1fa88c155
Mansi-Atey <mansiatey31@gmail.com>
2021-03-24 23:57:00 +0530

    [mansi]ADDED: code to create queue

--- CreateQueue/CreateQueue/Node.cs
+++ CreateQueue/CreateQueue/Node.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace PeekAndPop
+namespace CreateQueue
 {
     class Node
     {
--- CreateQueue/CreateQueue/Program.cs
+++ CreateQueue/CreateQueue/Program.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 
-namespace PeekAndPop
+namespace CreateQueue
 {
     class Program
     {
         static void Main(string[] args)
         {
+            //Queue Operations
+            Queue linkedListQueue = new Queue();
+            linkedListQueue.Enqueue(56);
+            linkedListQueue.Enqueue(30);
+            linkedListQueue.Enqueue(70);
+            linkedListQueue.Display();
+
+            //Stack Operations
             Stack stack = new Stack();
             stack.Push(70);
             stack.Push(30);
